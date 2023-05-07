6f04fe94c33bb85d7cd78e5e16996a26d34602f2
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2023-05-07 09:28:26 -0700

    add: favicon remove: template icons add: vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "BagelChat",
-	description: "Talk to anyone!"
+	description: "Talk to anyone!",
+	icons: {
+		apple: "/apple-touch-icon.png",
+		icon: [
+			{ sizes: "32x32", url: "/favicon-32x32.png" },
+			{ sizes: "16x16", url: "/favicon-16x16.png" }
+		]
+	},
+	manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({
