7f24983e7b65ebaaee6045a9ee9c37ad931330b3
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2023-05-06 21:19:44 -0700

    add: theme add: typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}"
 	],
 	theme: { extend: {} },
-	plugins: [require("daisyui")]
+	daisyui: {
+		themes: [
+			{
+				theme: {
+					"primary": "#75DBCD",
+					"secondary": "#003566",
+					"accent": "#218275",
+					"neutral": "#002D52",
+					"base-100": "#001427",
+					"info": "#38A1C7",
+					"success": "#06BA63",
+					"warning": "#F2CB07",
+					"error": "#FF220C"
+				}
+			}
+		]
+	},
+	plugins: [require("@tailwindcss/typography"), require("daisyui")]
 };
